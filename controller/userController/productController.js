f80aec415daf51bd025f398c2c3d5d4082c538f9
Asheem Rahman <mhdasheem@gmail.com>
2024-06-28 10:45:56 +0530

    Error fix

--- controller/userController/productController.js
+++ controller/userController/productController.js
@@ -5,8 +5,8 @@ const productSchema = require('../../model/productSchema')
 const productDetail = async (req, res) => {
    try {
       const id = req.params.id
-      const product = await productSchema.findById(id,{ isActive : true })
-      const similarProduct = await productSchema.find({productCollection: product.productCollection,isActive : true })
+      const product = await productSchema.findById(id)
+      const similarProduct = await productSchema.find({productCollection: product.productCollection})
       res.render('user/productDetail', {title: product.productName , product , similarProduct , user:req.session.user})
    } catch (error) {
    console.log(`error while rendering product page ${error}`)
