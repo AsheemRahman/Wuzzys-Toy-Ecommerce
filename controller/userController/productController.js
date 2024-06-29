efcfede029e976188a210e0b9588dc8fa026f097
Asheem Rahman <mhdasheem@gmail.com>
2024-06-29 12:33:48 +0530

    Similar product show

--- controller/userController/productController.js
+++ controller/userController/productController.js
@@ -6,7 +6,7 @@ const productDetail = async (req, res) => {
    try {
       const id = req.params.id
       const product = await productSchema.findById(id)
-      const similarProduct = await productSchema.find({productCollection: product.productCollection})
+      const similarProduct = await productSchema.find({productCollection: product.productCollection , isActive : true })
       res.render('user/productDetail', {title: product.productName , product , similarProduct , user:req.session.user})
    } catch (error) {
    console.log(`error while rendering product page ${error}`)
